60d4e2f4e827d70362777a55d658c023ef7e09d2
Ahian <190030514@qq.com>
2019-05-30 22:20:22 +0800

    init
    待完成：商品的css，动态的css

---
+++ build/precache-manifest.0b2aaabbff88c7200653e0f44fe008e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2836f0ecab02d7b55977",
+    "url": "/static/css/main.0cb173ab.chunk.css"
+  },
+  {
+    "revision": "2836f0ecab02d7b55977",
+    "url": "/static/js/main.e1df84e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9438fd934a6edb41bdef",
+    "url": "/static/css/2.57cf81d1.chunk.css"
+  },
+  {
+    "revision": "9438fd934a6edb41bdef",
+    "url": "/static/js/2.7ee2945a.chunk.js"
+  },
+  {
+    "revision": "54956034d723bcd2fa02ed5a38733de7",
+    "url": "/index.html"
+  }
+];
