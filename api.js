b77e022c9083dabca63e5818b159036b47e43a42
AlmondLee <754125579@qq.com>
2019-06-23 16:15:16 +0800

    api.md

---
+++ api.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+get('/get_seller_pyq', 'SellerController@getSellerPyq');
+
+
+
+
+
+get('/search_product', 'ProductsController@searchProduct');
+
+
+get('/logon', 'SellerController@logon');
+
+get('/login', 'SellerController@login');
+
+//在打开上传表单的页面的时候就要先请求请求此接口, 返回的id_product要保存在缓存里(如果没有提交表单的话下次还是用这个)
+get('/before_generate_products',{
+    token: 'rgdfsdrgdnhgnjtyd'
+});
+
+//
+get('/upload_product_url', {
+    token: "asfgrnjklhsrbfgtksgdf",
+    id_product: 1,
+    url: 'www',
+    content_type: 'tburl'
+});
+post('/upload_product_img', {
+    token: "asfgrnjklhsrbfgtksgdf",
+    id_product: 1,
+    file: P.jpg
+});
+
+
+// post('/login', 'SellerController@login');
+
+get('/generate_products', {
+    price: '惊爆价300',
+    product_name: '名称',
+    id_product: 1,
+    infs: '商品描述(long)',
+    token: 'fghjsdrf',
+    urls: JSON.stringify([
+        {
+            type: 'tburl',
+            content: 'www.taobao.com/dfgdfghgh'
+        },
+        {
+            type: 'jdurl',
+            content: 'www.jd.com/dasfasfdg'
+        }
+    ])
+});
+
+get('/get_products', {
+    id_seller: 1,   //必填
+    lastProduct: 'false',
+    order: 'updated_at',
+    orderValue: 1,
+    esc: 'true'
+});
+
+//
+get('/delete_product_file', {
+    token: 'sfgdazrg',
+    id_product: 1,
+    product_img_id: 2
+});
+
+get('/get_simple_products', 'ProductsController@getSimpleProducts');
+
+get('/get_seller_info', 'SellerController@getSeller');
+
+get('/upload_seller_img', 'ProductsController@uploadSellerImg');
