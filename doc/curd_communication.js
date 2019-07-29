f48695013fd948a6ea9720d41e095c2e94c1d08a
AlmondLee <754125579@qq.com>
2019-07-29 14:16:05 +0800

    api

--- doc/curd_communication.js
+++ doc/curd_communication.js
@@ -11,16 +11,24 @@ post('/curd_communication', {
 return '{"success":"\u589e\u6dfb\u6210\u529f","Communication":{"name":"","phone":"","qq":"","wx_id":"","locate":"god","id_openid":"9","updated_at":"1564379626","created_at":"1564379626","id":7}}'
 
 
+
 post('/curd_communication', {
     toekn: '',
-    method: 'worker_get',
-    id_seller: 1
+    method: 'worker_get',   //这个就是接单后工作者获取联系方式的方法
+    id_seller: 1    //把get_works返回的id_openid放在这里
 });
+
+
+
 post('/curd_communication', {
     toekn: '',
     method: 'delete',
     id_communication: 1,
 });
+return '{"success":"\u5220\u9664\u6210\u529f"}'
+
+
+
 post('/curd_communication', {
     toekn: '',
     method: 'update',     //更新某一组联系方式
