3a2943badb5f22267dd092e8ad3e4ad53209c759
AlmondLee <754125579@qq.com>
2019-07-30 15:41:38 +0800

    api

---
+++ doc/delete_file.js
@@ -0,0 +1,11 @@
+var params = {
+    token: '',
+    file_name: '/storage/fgjhvdjklfdsKhnbagvkjhnkjgrvfh.png',    //数据库中的content字段
+    type: 'work',                                               //另一种是worker, 分别修改发布和上传凭证时的图片
+    id_work: 5,                                                 //与之关联的任务的id
+}
+
+
+function get_works_by_me (params) {
+
+}
---
+++ doc/upload_worker_file.js
@@ -0,0 +1,10 @@
+var params = {
+    id_work: 5, //接受的任务的id
+    token: '',
+    file: 文件
+}
+
+
+function upload_worker_file (params) {
+
+}
