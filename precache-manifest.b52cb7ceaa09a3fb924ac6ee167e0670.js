9009e39c4c217ce2cb7add3c11767f5f071fb502
Ahian <190030514@qq.com>
2019-08-03 23:53:48 +0800

    金币

---
+++ precache-manifest.b52cb7ceaa09a3fb924ac6ee167e0670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de0b489a7ce1d1d308d0",
+    "url": "/static/css/main.043ef590.chunk.css"
+  },
+  {
+    "revision": "de0b489a7ce1d1d308d0",
+    "url": "/static/js/main.edbbb1c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6251e6db1f25ef889ade",
+    "url": "/static/css/2.7c913ce7.chunk.css"
+  },
+  {
+    "revision": "6251e6db1f25ef889ade",
+    "url": "/static/js/2.5b43fe79.chunk.js"
+  },
+  {
+    "revision": "f2b814cd77cace3b81a9332749c9d4d0",
+    "url": "/index.html"
+  }
+];
