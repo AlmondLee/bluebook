8bf53b8d0fb1f82af55ebc27222dba2e8fe4e430
Ahian <190030514@qq.com>
2019-08-15 14:59:59 +0800

    sort

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b133f7c839d94eabfe86a4bd17eecd09.js"
+  "/precache-manifest.9448c309b8ecf2eb5026fe9634bfa28d.js"
 );
 
 workbox.clientsClaim();
