df3275d4460658c47fe0ecce1740994a1619b42f
Ahian <190030514@qq.com>
2019-08-17 20:13:24 +0800

    p

---
+++ precache-manifest.b7963ac6cdfbf09312f3590b57740b52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29a7a8b079caee0e8ff5",
+    "url": "/static/css/main.a09ed0dc.chunk.css"
+  },
+  {
+    "revision": "29a7a8b079caee0e8ff5",
+    "url": "/static/js/main.4c7e2240.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6d769673e4305166267",
+    "url": "/static/css/2.296ca6d8.chunk.css"
+  },
+  {
+    "revision": "d6d769673e4305166267",
+    "url": "/static/js/2.1d6b25b8.chunk.js"
+  },
+  {
+    "revision": "6d3b512f0488e2e010a22ee996db21af",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60339c2fd92b6113734b6c16e27fd614.js"
+  "/precache-manifest.b7963ac6cdfbf09312f3590b57740b52.js"
 );
 
 workbox.clientsClaim();
