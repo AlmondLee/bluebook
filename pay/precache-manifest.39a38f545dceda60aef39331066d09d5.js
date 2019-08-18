a1539b183f33ce4cdf9446e0d09dadae79d3afc4
Ahian <190030514@qq.com>
2019-08-18 15:56:36 +0800

    pay

---
+++ pay/precache-manifest.39a38f545dceda60aef39331066d09d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "755e34f9369d0aa93f80",
+    "url": "/static/css/main.a09ed0dc.chunk.css"
+  },
+  {
+    "revision": "755e34f9369d0aa93f80",
+    "url": "/static/js/main.2729fd23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6d769673e4305166267",
+    "url": "/static/css/2.296ca6d8.chunk.css"
+  },
+  {
+    "revision": "d6d769673e4305166267",
+    "url": "/static/js/2.1d6b25b8.chunk.js"
+  },
+  {
+    "revision": "67b66b9ed36f389a0e332dbac3ca6ef8",
+    "url": "/index.html"
+  }
+];
